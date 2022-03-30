3510228202423b18eb2e839b37b7c8ee67cede58
Matej-Matejcik <Matej.Matejcik@ness.com>
2022-03-30 11:16:55 +0200

    added author stats functionality (GET)

--- src/main/java/sk/ness/academy/dao/AuthorDAO.java
+++ src/main/java/sk/ness/academy/dao/AuthorDAO.java
@@ -3,11 +3,15 @@ package sk.ness.academy.dao;
 import java.util.List;
 
 import sk.ness.academy.dto.Author;
+import sk.ness.academy.dto.AuthorStats;
 
 public interface AuthorDAO {
 
   /** Returns all available {@link Author}s */
   List<Author> findAll();
+
+  /** Returns list of all authors with their number of articles */
+  List<AuthorStats> getAuthorStats();
 
 }
 
--- src/main/java/sk/ness/academy/service/AuthorService.java
+++ src/main/java/sk/ness/academy/service/AuthorService.java
@@ -3,10 +3,14 @@ package sk.ness.academy.service;
 import java.util.List;
 
 import sk.ness.academy.dto.Author;
+import sk.ness.academy.dto.AuthorStats;
 
 public interface AuthorService {
 
 	  /** Returns all available {@link Author}s */
 	  List<Author> findAll();
+
+	/** Returns list of all authors with their number of articles */
+	List<AuthorStats> getAuthorStats();
 
 	}
